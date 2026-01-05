44deb4972633a786ae8308df85ba01430e3db410
Emmanuel Okoh <110433564+otizgit@users.noreply.github.com>
2026-01-05 22:40:17 +0100

    illustration chat icons addition

--- src/components/interface/authContainers/IllustrationContainer.tsx
+++ src/components/interface/authContainers/IllustrationContainer.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import clouds from "@/assets/images/sky.jpg";
 import avatar1 from "@/assets/images/avatar1.svg";
 import avatar2 from "@/assets/images/avatar2.svg";
+import chatIcons from "@/assets/images/chat-icons.png";
 import chatImg from "@/assets/images/chat-img.png";
 import { Icon } from "@iconify/react";
 
@@ -18,18 +19,18 @@ export default function IllustrationContainer({
         <h1 className="text-white leading-7.5 mb-1 font-semibold text-[1.4rem]">
           {title}
         </h1>
-        <p className="text-gray-100 text-small mb-12 w-100 mx-auto">
+        <p className="text-gray-100 text-small mb-8 w-100 mx-auto">
           {description}
         </p>
 
         <div className="rounded-xl relative bg-black/30 w-[86%] 2xl:w-100 mx-auto shadow-sm border- pb-5 border-[0.3em] border-black/30">
-          {/* <Image
+          <Image
             draggable={false}
-            className="w-35 absolute -right-15 top-3 -scale-x-100"
-            src={avatar1}
+            className="w-65 absolute -right-15 -top-6"
+            src={chatIcons}
             alt="chat 3d icon"
           />
-          <Image
+          {/* <Image
             draggable={false}
             className="w-35 absolute bottom-10 -left-15"
             src={avatar2}
@@ -43,40 +44,38 @@ export default function IllustrationContainer({
           </div>
 
           <div className="px-4">
-            <div className="mb-4">
+            <div className="mb-6">
               <div className="mb-2 relative w-60 bg-primary plain-flex flex-col items-start gap-2 rounded-xl p-3">
                 <div className="flex items-end justify-center w-11 h-11 overflow-hidden bg-white border-[0.1em] border-black/80 rounded-full absolute -top-2 -left-1">
-                    <Image className="w-9" src={avatar1} alt="male avatar" />
-                  </div>
-                <span className="absolute -bottom-3 left-3 w-0 h-0 border-t-10 border-b-10 border-l-20 rotate-90 border-t-transparent border-b-transparent border-l-primary"></span>
+                  <Image className="w-9" src={avatar1} alt="male avatar" />
+                </div>
+                <span className="absolute -bottom-3 left-3 w-0 h-0 border-t-10 border-b-20 border-l-25 rotate-360 border-t-transparent border-b-transparent border-l-primary"></span>
                 <span className="w-[90%] h-1.5 rounded-full bg-white"></span>
                 <span className="w-[60%] h-1.5 rounded-full bg-white"></span>
-                <span className="w-[40%] h-1.5 rounded-full bg-white"></span>
+                <span className="w-[40%] h-1.5 rounded-full bg-white relative z-1"></span>
               </div>
-              <Icon
-                icon="icon-park-outline:check-one"
-                className="text-small text-primary"
-              />
+              {/* <div className="pl-6">
+                <Icon
+                  icon="icon-park-outline:check-one"
+                  className="text-small text-primary"
+                />
+              </div> */}
             </div>
 
-            <div className="mb-4">
+            <div className="mb-8">
               <div className="relative w-60 ml-auto bg-primary plain-flex flex-col items-start gap-2 rounded-xl p-3 mb-2">
-                <div className="flex items-end justify-center w-11 h-11 overflow-hidden bg-white border-[0.1em] border-black/80 rounded-full absolute -top-2 -right-1">
-                    <Image
-                      className="w-8.5"
-                      src={avatar2}
-                      alt="image of a phone"
-                    />
-                  </div>
-                <span className="absolute -bottom-3 right-3 w-0 h-0 border-t-10 border-b-10 border-l-20 rotate-90 border-t-transparent border-b-transparent border-l-primary"></span>
+                <div className="flex items-end justify-center w-11 h-11 overflow-hidden bg-white border-[0.1em] border-black/80 rounded-full absolute -top-2 -right-1 z-1">
+                  <Image
+                    className="w-8.5"
+                    src={avatar2}
+                    alt="image of a phone"
+                  />
+                </div>
+                <span className="absolute -bottom-3 right-3 w-0 h-0 border-t-10 border-b-20 border-r-25 border-t-transparent border-b-transparent border-r-primary"></span>
                 <span className="w-[25%] h-1.5 rounded-full bg-white"></span>
                 <span className="w-[55%] h-1.5 rounded-full bg-white"></span>
                 <span className="w-[85%] h-1.5 rounded-full bg-white"></span>
               </div>
-              <Icon
-                icon="icon-park-outline:check-one"
-                className="text-small text-primary ml-auto"
-              />
             </div>
 
             <div className="bg-white/20 rounded-lg px-3 custom-flex">
